799c82de9e96ee85f998c7b8702e5a47678db124
arkayz <arkayzeol@gmail.com>
2021-03-07 23:33:20 +0100

    .

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,25 +1,31 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-
-  final estiloTexto = new TextStyle (fontSize: 25);
+  final estiloTexto = new TextStyle(fontSize: 25);
+  final conteo = 10;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Titulo'),
-          centerTitle: true,
-        ),
-        body: Center(
+      appBar: AppBar(
+        title: Text('Titulo'),
+        centerTitle: true,
+      ),
+      body: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Número de clicks:', style: estiloTexto ,),
-              Text('0', style: estiloTexto),
-            ],
-          )
-        ),
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Número de clicks:',
+            style: estiloTexto,
+          ),
+          Text('$conteo', style: estiloTexto),
+        ],
+      )),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
     );
   }
 }
